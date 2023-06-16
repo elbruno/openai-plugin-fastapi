e0f5c8636c046801bc00aa8e5db38f75506ec6eb
El Bruno <elbruno@users.noreply.github.com>
2023-06-16 15:33:55 +0000

    add pet search

--- main.py
+++ main.py
@@ -7,20 +7,37 @@ app = FastAPI()
 app.include_router(wellknown)
 app.add_middleware(CORSMiddleware, allow_origins=["https://chat.openai.com"])
 
-with open("./data/products.json", "r") as f:
-    products = json.load(f)
+# with open("./data/products.json", "r") as f:
+#     products = json.load(f)
+
+with open("./data/pets.json", "r") as f:
+    pets = json.load(f)
 
 
-@app.get("/products", summary="Get a list of products", operation_id="getProducts")
-async def get_products(query: str = None):
+@app.get("/pets", summary="Get a list of pets", operation_id="getPets")
+async def get_pets(query: str = None):
     """
-    Returns a list of products, optionally filtered by providing a query parameter.
+    Returns a list of pets, optionally filtered by providing a query parameter.
     """
     if query:
         keywords = query.lower().split()
         return [
-            product
-            for product in products
-            if all(keyword in str(product.values()).lower() for keyword in keywords)
+            pet
+            for pet in pets
+            if all(keyword in str(pet.values()).lower() for keyword in keywords)
         ]
-    return products
+    return pets
+
+# @app.get("/products", summary="Get a list of products", operation_id="getProducts")
+# async def get_products(query: str = None):
+#     """
+#     Returns a list of products, optionally filtered by providing a query parameter.
+#     """
+#     if query:
+#         keywords = query.lower().split()
+#         return [
+#             product
+#             for product in products
+#             if all(keyword in str(product.values()).lower() for keyword in keywords)
+#         ]
+#     return products
